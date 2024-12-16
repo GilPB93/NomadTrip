aad3b83e19f2bc9dfc7432e3bde67c063303e1b9
GilPB93 <g.p.braga@hotmail.com>
2024-12-16 19:12:22 +0100

    add signin & signup pages

--- NomadTripFrontEnd/Router/allRoutes.js
+++ NomadTripFrontEnd/Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "./Route.js";
 //Définir ici les routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
 
 
 ];

969a6044056b8cf6095574c6058877331d8ddd05
GilPB93 <g.p.braga@hotmail.com>
2024-12-17 10:19:26 +0100

    add all pages+links

--- NomadTripFrontEnd/Router/allRoutes.js
+++ NomadTripFrontEnd/Router/allRoutes.js
@@ -3,9 +3,12 @@ import Route from "./Route.js";
 //Définir ici les routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/signup", "Inscription", "/pages/signup.html"),
     new Route("/signin", "Connexion", "/pages/signin.html"),
-
+    new Route("/account", "Mon Compte", "/pages/account.html"),
+    new Route("/admin", "Mon Espace Admin", "/pages/admin.html"),
+    new Route("/library", "Mes Carnets", "/pages/library.html"),
 
 ];
 

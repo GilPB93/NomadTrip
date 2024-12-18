70ad0dd3f8c9cd993c1f58d34d8a287e1933dc5e
GilPB93 <g.p.braga@hotmail.com>
2024-12-18 16:01:26 +0100

    add modals and collaps on library page

--- NomadTripFrontEnd/js/library.js
+++ NomadTripFrontEnd/js/library.js
@@ -3,16 +3,15 @@
 const collapseCreateTravelbook = document.getElementById('collapseCreateTravelbook');
 const newTravelbookForm = document.getElementById('newTravelbookForm');
 
-collapseCreateTravelbook.addEventListener('hidden.bs.collapse', 
-    function () {
+collapseCreateTravelbook.addEventListener('hidden.bs.collapse', function () {
         newTravelbookForm.reset()
 });
 
-//FUNCTION TO CLOSE DE MODAL
+//FUNCTION TO CLOSE DE MODAL & ERASE THE FORM
 const EditionTravelbookModal = document.getElementById('EditionTravelbookModal');
 const editTravelbookForm = document.getElementById('editTravelbookForm');
 
-EditionTravelbookModal.addEventListener('hidden.bs.modal',
-    function () {
+EditionTravelbookModal.addEventListener('hidden.bs.modal', function () {
         editTravelbookForm.reset();
-        });
+});
+

9a224d597332265127f844072073684a8966c5de
GilPB93 <g.p.braga@hotmail.com>
2025-02-08 12:34:46 +0100

    ContactMessageFixtures ok

---
+++ src/DataFixtures/ContactMessageFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ContactMessage;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ContactMessageFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        for ($i = 1; $i <= 20; $i++) {
+            $contactMessage = (new ContactMessage())
+                ->setName($faker->firstName)
+                ->setEmail($faker->email)
+                ->setSubject($faker->sentence)
+                ->setMessage($faker->paragraph)
+                ->setSentAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('now', '+1 year')));
+            $manager->persist($contactMessage);
+        }
+
+        $manager->flush();
+    }
+}

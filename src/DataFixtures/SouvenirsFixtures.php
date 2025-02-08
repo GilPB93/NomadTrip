c1d1c78f2a7351ec207e6416268baf6c8f267bc0
GilPB93 <g.p.braga@hotmail.com>
2025-02-08 12:15:28 +0100

    SouvenirsFixtures ok

---
+++ src/DataFixtures/SouvenirsFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Souvenirs;
+use App\Entity\Travelbook;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class SouvenirsFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+        $travelbooks = $manager->getRepository(Travelbook::class)->findAll();
+        if (empty($travelbooks)) {
+            throw new \Exception('No users found. Please load TravelbookFixtures first.');
+        }
+
+        for ($i = 1; $i <= 20; $i++) {
+            $souvenir = (new Souvenirs())
+                ->setWhat($faker->word)
+                ->setForWho($faker->firstName)
+                ->setTravelbook($faker->randomElement($travelbooks));
+            $manager->persist($souvenir);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            TravelbookFixtures::class,
+        ];
+    }
+}

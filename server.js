e648572046e6ca944267723904ee4f61e40e1975
GilPB93 <g.p.braga@hotmail.com>
2025-03-05 15:49:19 +0100

    add express.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Servir les fichiers statiques du dossier public
+app.use(express.static(path.join(__dirname)));
 
+// Si aucune route ne correspond, renvoyer index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Définir le port Heroku
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Serveur en ligne sur le port ${PORT}`));

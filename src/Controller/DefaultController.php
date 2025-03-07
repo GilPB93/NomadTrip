d586d47a611ceecbe408300f9b2726cd201f69cb
GilPB93 <g.p.braga@hotmail.com>
2025-03-07 17:00:27 +0100

    redirect pages to frontend

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,4 +13,17 @@ class DefaultController extends AbstractController
     {
         return new Response("Bienvenue sur votre accueil!") ;
     }
+
+    #[Route('/{slug}', name: 'frontend', requirements: ['slug' => '^(?!api).*$'])]
+    public function index(): Response
+    {
+        $filePath = $this->getParameter('kernel.project_dir').'/public/frontend/index.html';
+        
+        if (!file_exists($filePath)) {
+            return new Response("Le fichier frontend/index.html est introuvable", 404);
+        }
+
+        return new Response(file_get_contents($filePath));
+    }
+
 }
